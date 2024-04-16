864cc7a8bd3ad4b60e734ad5005e1c8a9fa16778
apm30 <m.ajaypradeep@icloud.com>
2024-04-16 15:15:03 +0100

    project init

---
+++ prettier.config.js
@@ -0,0 +1,27 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/** @type {import("prettier").Config} */
+const config = {
+    trailingComma: 'all',
+    tabWidth: 4,
+    printWidth: 80,
+    semi: true,
+    singleQuote: true,
+    importOrder: [
+        '<BUILTIN_MODULES>',
+        '^(^react$|@react|react)',
+        '^(next/(.*)$)|^(next$)',
+        '<THIRD_PARTY_MODULES>',
+
+        '^@/(components|data|config)/',
+        '^[./]',
+    ],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    plugins: [
+        '@trivago/prettier-plugin-sort-imports',
+        'prettier-plugin-tailwindcss',
+    ],
+};
+
+export default config;
---
+++ src/components/NavBar/MobileNav.tsx
@@ -0,0 +1,3 @@
+export default function MobileNav() {
+    return null;
+}
